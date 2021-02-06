750e8c54c0b0fd4893392efed744b223e91057ad
daihy <dai875939260@163.com>
2021-02-06 19:29:51 +0800

    :bug: Fix Path Problem

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-import { join } from 'path';
-
 export default () => {
     return {
         name: 'native-plugin',
         async transform(src: any, id: string) {
             if (id.endsWith('.node')) {
-                const dir = join(process.cwd(), id);
                 return `
-                    const m = { exports: {} };
-                    process.dlopen(
-                        m,
-                        '${dir}'
-                    )
-                    export default m.exports;
+                    try {
+                        global.process.dlopen(module, '${id}');
+                    } catch(exception) {
+                        throw new Error('Cannot open  ${id} :'+ exception);
+                    }
+                    export default module.exports;
                 `;
             }
         },
